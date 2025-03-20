40d114c426f77d963e5d6c6574101fe1dfde3941
mittei <181688194+mittei@users.noreply.github.com>
2025-03-20 21:33:58 +0900

    fix my stupid bugs which made the EN tokenizer not work

--- js/load-lunr-plugin.js
+++ js/load-lunr-plugin.js
@@ -7,7 +7,7 @@
 // copied from: https://github.com/MihaiValentin/lunr-languages/blob/master/lunr.ja.js
 elasticlunr.jp.wordCharacters = "一二三四五六七八九十百千万億兆一-龠々〆ヵヶぁ-んァ-ヴーｱ-ﾝﾞa-zA-Zａ-ｚＡ-Ｚ0-9０-９";
 
-var re_jpchar = new RegExp("[" + elasticlunr.jp.wordCharacters + "]")
+var re_ENchar = new RegExp("[" + elasticlunr.wordCharacters + "]")
 
 // Patch load method, which is used by searcher.js.
 // This swaps the prebuilt index with a new index 
@@ -25,20 +25,33 @@ elasticlunr.Index.load = function (index) {
 
         // first tokenize with the English tokenizer, and then 
         // only apply the Japanese tokenizer on tokens that 
-        // include any Japanese characters.
+        // don't look like English.
+        //
+        // Why should we not use just the JP tokenizer?
+        // Because a query like "they're" would be 
+        // tokenized into "they" and "re" and 
+        // EN stopWordFilter removes "they" and 
+        // you're left with "re" which would match
+        // any words that start with re.
         elasticlunr.tokenizer = function (obj) {
             var result = [];
             var en_tokens = en_tokenizer(obj);
             for (var i = 0; i < en_tokens.length; i++) {
-                if (en_tokens[i].match(re_jpchar)) {
-                    var jp_tokens = elasticlunr.jp.tokenizer(obj);
-                    result.push.apply(result, jp_tokens);
-                } else {
+                if (en_tokens[i].match(re_ENchar)) {
                     result.push(en_tokens[i]);
+                } else {
+                    var jp_tokens = elasticlunr.jp.tokenizer(en_tokens[i]);
+                    result.push.apply(result, jp_tokens);
                 }
             }
             return result;
         };
+
+        // At least the `seperator` property is required for
+        // EN tokenizer to work correctly
+        for (var prop in en_tokenizer) {
+            elasticlunr.tokenizer[prop] = en_tokenizer[prop]
+        }
 
         // fields to index
         this.addField('title');
